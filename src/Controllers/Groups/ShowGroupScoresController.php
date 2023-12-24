e211af0a88a86a2caf69a06061ce591fb2d390a8
Sebastiaan Kloos <sebastiaan@codecycler.com>
2023-12-24 13:05:01 +0100

    Add course details and user information to ShowGroupScoresController
    Extended the ShowGroupScoresController to include details about each course and additional user information in the output, improving data granularity. Updated ShowGroupScoresTest accordingly to reflect these changes, ensuring test coverage remains thorough.
    
    Signed-off-by: Sebastiaan Kloos <sebastiaan@codecycler.com>

--- src/Controllers/Groups/ShowGroupScoresController.php
+++ src/Controllers/Groups/ShowGroupScoresController.php
@@ -20,21 +20,47 @@ class ShowGroupScoresController extends Controller
                     ->whereIn('course_id', $courseIds);
             }])
             ->get()
-            ->map(function (LmsUser $user) use ($courseCount) {
+            ->map(function (LmsUser $user) use ($team, $courseCount) {
                 $relativeScores = $user->courseScores->pluck('relative_score')->values()->toArray();
 
                 $scores = array_sum($relativeScores);
 
                 $globalRelativeScore = ($courseCount === 0) ? 0 : $scores / $courseCount;
 
+                $courses = $team->courses
+                    ->map(function ($course) use ($user) {
+                        $courseScore = $user->courseScores->firstWhere('course_id', $course->id);
+
+                        $state = false;
+                        $relativeScore = 0;
+
+                        if ($courseScore) {
+                            $state = $courseScore['achieved'] ? true : false;
+                            $relativeScore = $courseScore['relative_score'];
+                        }
+
+                        return [
+                            'id' => $course->id,
+                            'name' => $course->name,
+                            'achieved' => $state,
+                            'score' => $relativeScore * 100,
+                        ];
+                    })
+                    ->toArray();
+
                 return [
                     'id' => $user->id,
                     'name' => $user->name,
                     'email' => $user->email,
                     'phone' => $user->phone,
-                    'score' => $globalRelativeScore,
+                    'lms_roles' => $user->lms_roles,
+                    'last_seen_at' => $user->last_seen_at,
+                    'created_at' => $user->created_at,
+                    'score' => $globalRelativeScore * 100,
+                    'courses' => $courses,
                 ];
-            });
+            })
+            ->toArray();
 
         return [
             'data' => [
--- tests/src/Controllers/ShowGroupScoresTest.php
+++ tests/src/Controllers/ShowGroupScoresTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Laravel\Sanctum\Sanctum;
+use LearnKit\Lms\Models\Course;
 use LearnKit\Lms\Models\Group;
 use LearnKit\LmsConnect\Tests\Models\User;
 use LearnKit\LmsConnect\Tests\TestCase;
@@ -11,16 +12,44 @@ uses(TestCase::class);
 it('can return all users in a group with their scores', function () {
     $team = createTeamAndActivatePlugin();
 
+    $users = User::factory()->count(5)->create();
+
+    $team->users()->sync($users);
+
+    $courses = Course::factory()->count(10)->create();
+
+    $team->courses()->sync($courses);
+
     $group = Group::factory()
         ->for($team)
-        ->has(
-            User::factory()->count(5)
-        )
+        ->hasAttached($users)
         ->create();
 
-    $user = User::factory()->create();
+    // Simulate a course completion for the first user
+    $user = $team->users()->first();
+    $course = $team->courses()->first();
 
-    Sanctum::actingAs($user);
+    $user->courseScores()
+        ->create([
+            'course_id' => $course->id,
+            'score' => 5,
+            'max_score' => 5,
+            'achieved' => true,
+        ]);
+
+    // Simulate second course partially completed
+    $course = $team->courses()->get()[1];
+
+    $user->courseScores()
+        ->create([
+            'course_id' => $course->id,
+            'score' => 3,
+            'max_score' => 10,
+            'achieved' => false,
+        ]);
+
+    //
+    Sanctum::actingAs($users[0]);
 
     $response = getJson(route('lms-connect.team.groups.group.scores', ['team' => $team->slug, 'group' => $group]));
 
@@ -31,5 +60,7 @@ it('can return all users in a group with their scores', function () {
         ->and($response->json('data.users'))
         ->toHaveCount(5)
         ->and($response->json('data.users')[0])
+        ->ray()
         ->toHaveKeys(['id', 'name', 'email', 'phone', 'score']);
 });
+
